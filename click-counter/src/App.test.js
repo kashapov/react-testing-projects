68c51da5622578488b7e07cc4f2036b12fadb265
Andrey Kashapov <kashapovandrey@gmail.com>
2019-12-04 14:01:36 +0200

    init: init project, jest & enzyme

---
+++ click-counter/src/App.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+
+import App from "./App";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders without error", () => {});
+
+test("renders increment button", () => {});
+
+test("renders counter display", () => {});
+
+test("counter starts from 0", () => {});
+
+test("click button increments counter display", () => {});
