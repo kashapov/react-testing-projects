081d06ab4aa07f013f5ece485ecea80ef4d74694
Andrey Kashapov <kashapovandrey@gmail.com>
2019-12-04 16:50:01 +0200

    init project, jest & enzyme

---
+++ jotto-redux/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div data-test="component-app" className="App">
+      App
+    </div>
+  );
+}
+
+export default App;
