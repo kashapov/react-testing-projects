6a81fbf90830fbd8a29d152f6d7e6e3ed030773a
Andrey Kashapov <kashapovandrey@gmail.com>
2019-12-04 17:54:15 +0200

    feat: add prop-types for Congrats component

--- jotto-redux/test/testUtils.js
+++ jotto-redux/test/testUtils.js
@@ -1,3 +1,5 @@
+import checkPropTypes from "check-prop-types";
+
 /**
  * Return ShallowWrapper containing node(s) with the given data-test value.
  * @function setup
@@ -7,4 +9,15 @@
  */
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`);
+};
+
+export const checkProps = (component, conformingProps) => {
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    "prop",
+    component.name
+  );
+
+  expect(propError).toBeUndefined();
 };
