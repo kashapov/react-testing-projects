e8d62775f112a121dddad2f5a0924d1bcb75fd90
Andrey Kashapov <kashapovandrey@gmail.com>
2019-12-05 14:57:20 +0200

    feat: base GuessedWords component and tests

--- jotto-redux/src/App.js
+++ jotto-redux/src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 
+import GuessedWords from "./GuessedWords";
+import Congrats from "./Congrats";
+
 import "./App.css";
 
 function App() {
   return (
     <div data-test="component-app" className="App">
-      App
+      <h1>Jotto</h1>
+      <Congrats success={true} />
+      <GuessedWords
+        guessedWords={[
+          { guessedWord: "train", letterMatchCount: 3 },
+          { guessedWord: "agile", letterMatchCount: 1 },
+          { guessedWord: "party", letterMatchCount: 5 }
+        ]}
+      />
     </div>
   );
 }
--- jotto-redux/src/Congrats.js
+++ jotto-redux/src/Congrats.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types";
 const Congrats = props => {
   if (props.success) {
     return (
-      <div data-test="component-congrats" className="alert alert-success">
+      <div data-test="component-congrats">
         <span data-test="congrats-message">
           Congratulations! You guessed the word!
         </span>
