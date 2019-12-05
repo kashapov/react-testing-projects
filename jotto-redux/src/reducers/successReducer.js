d21a8906ee8d0c936fe3e9824efd26b88d31bdfb
Andrey Kashapov <kashapovandrey@gmail.com>
2019-12-05 16:13:27 +0200

    feat: successReducer and test

--- jotto-redux/src/reducers/successReducer.js
+++ jotto-redux/src/reducers/successReducer.js
@@ -1,9 +1,17 @@
+import { actionTypes } from "../actions/";
+
 /**
  * @function successReducer
  * @param {boolean} state - Whether the user has guessed correctly.
  * @param {object} action - Action to be reduced.
  * @returns {boolean} - New success state.
  */
-export default (state, action) => {
-  return null;
+export default (state = false, action) => {
+  switch (action.type) {
+    case actionTypes.CORRECT_GUESS:
+      return true;
+
+    default:
+      return state;
+  }
 };
