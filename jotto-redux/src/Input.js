6cf5d1fdb058eec79e1c05289b33aac44f49515d
Andrey Kashapov <kashapovandrey@gmail.com>
2019-12-05 17:35:41 +0200

    feat: add test for Input component

--- jotto-redux/src/Input.js
+++ jotto-redux/src/Input.js
@@ -3,16 +3,31 @@ import { connect } from "react-redux";
 
 class Input extends Component {
   render() {
-    return (
-      <div>
-        <button></button>
-      </div>
+    const contents = this.props.success ? null : (
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          id="word-guess"
+          type="text"
+          placeholder="enter guess"
+        />
+        <button
+          data-test="submit-button"
+          type="submit"
+          className="btn btn-primary mb-2"
+        >
+          Submit
+        </button>
+      </form>
     );
+
+    return <div data-test="component-input">{contents}</div>;
   }
 }
 
-const mapStateTpProps = state => {
-  return {};
+const mapStateTpProps = ({ success }) => {
+  return { success };
 };
 
 export default connect(mapStateTpProps)(Input);
