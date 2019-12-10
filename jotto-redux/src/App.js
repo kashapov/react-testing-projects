843c78ba8601ab9dc04a9a0057cde4e19c57ff07
Andrey Kashapov <kashapovandrey@gmail.com>
2019-12-10 12:09:27 +0200

    connect App component and tests

--- jotto-redux/src/App.js
+++ jotto-redux/src/App.js
@@ -1,26 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
+import Input from "./Input";
 import GuessedWords from "./GuessedWords";
 import Congrats from "./Congrats";
+import { getSecretWord } from "./actions";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div data-test="component-app" className="App container">
-      <h1>Jotto</h1>
-      <Congrats success={false} />
-      <GuessedWords
-        guessedWords={
-          [
-            // { guessedWord: "train", letterMatchCount: 3 },
-            // { guessedWord: "agile", letterMatchCount: 1 },
-            // { guessedWord: "party", letterMatchCount: 5 }
-          ]
-        }
-      />
-    </div>
-  );
+class App extends Component {
+  render() {
+    const { success, guessedWords } = this.props;
+
+    return (
+      <div data-test="component-app" className="App container">
+        <h1>Jotto</h1>
+        <Congrats success={success} />
+        <Input />
+        <GuessedWords guessedWords={guessedWords} />
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  const { success, guessedWords, secretWord } = state;
+  return { success, guessedWords, secretWord };
+};
+
+export default connect(mapStateToProps, { getSecretWord })(App);
--- jotto-redux/src/App.test.js
+++ jotto-redux/src/App.test.js
@@ -1,37 +1,45 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
-import EnzymeAdapter from "enzyme-adapter-react-16";
+import { shallow } from "enzyme";
 
 import App from "./App";
-
-Enzyme.configure({ adapter: new EnzymeAdapter() });
+import { storeFactory } from "../test/testUtils";
 
 /**
  * Factory function to create a ShallowWrapper for the App component.
  * @function setup
- * @param {object} props - Component props specific to this setup.
  * @param {object} state - Initial state for setup.
  * @return {ShallowWrapper}
  */
-const setup = (props = {}, state = null) => {
-  const wrapper = shallow(<App {...props} />);
-  if (state) wrapper.setState(state);
+const setup = (state = {}) => {
+  const store = storeFactory(state);
+  const wrapper = shallow(<App store={store} />)
+    .dive()
+    .dive();
   return wrapper;
 };
 
-/**
- * Return ShallowWrapper containing node(s) with the given data-test value.
- * @function setup
- * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within.
- * @param {string} val - Value of data-test attribute for search
- * @return {ShallowWrapper}
- */
-const findByTestAttr = (wrapper, val) => {
-  return wrapper.find(`[data-test="${val}"]`);
-};
-
-test("renders without error", () => {
-  const wrapper = setup();
-  const appComponent = findByTestAttr(wrapper, "component-app");
-  expect(appComponent.length).toBe(1);
+describe("redux props", () => {
+  test("has access to `success` state", () => {
+    const success = true;
+    const wrapper = setup({ success });
+    const successProp = wrapper.instance().props.success;
+    expect(successProp).toBe(success);
+  });
+  test("has access to `secretWord` state", () => {
+    const secretWord = "party";
+    const wrapper = setup({ secretWord });
+    const secretWordProp = wrapper.instance().props.secretWord;
+    expect(secretWordProp).toBe(secretWord);
+  });
+  test("has access to `guessedWords` state", () => {
+    const guessedWords = [{ guessedWord: "train", letterMatchCount: 3 }];
+    const wrapper = setup({ guessedWords });
+    const guessedWordsProp = wrapper.instance().props.guessedWords;
+    expect(guessedWordsProp).toBe(guessedWords);
+  });
+  test("`getSecretWord` action creator is a function prop", () => {
+    const wrapper = setup();
+    const getSecretWordProp = wrapper.instance().props.getSecretWord;
+    expect(getSecretWordProp).toBeInstanceOf(Function);
+  });
 });
