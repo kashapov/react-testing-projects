21580574a085bc6e1a9510f7c4449b869dc51e45
Andrey Kashapov <kashapovandrey@gmail.com>
2019-12-13 12:19:20 +0200

    feat: add Give Up functionality

--- jotto-redux/src/components/App/App.js
+++ jotto-redux/src/components/App/App.js
@@ -6,6 +6,7 @@ import GuessedWords from "../GuessedWords/GuessedWords";
 import Congrats from "../Congrats/Congrats";
 import TotalGuesses from "../TotalGuesses/TotalGuesses";
 import NewWordButton from "../NewWordButton/NewWordButton";
+import SecretWordReveal from "../SecretWordReveal/SecretWordReveal";
 
 import { getSecretWord, resetGame } from "../../actions";
 
@@ -17,16 +18,17 @@ export class UnconnectedApp extends Component {
   }
 
   render() {
-    const { success, guessedWords, resetGame } = this.props;
+    const { success, guessedWords, resetGame, gaveUp, secretWord } = this.props;
 
     return (
       <div data-test="component-app" className="App container">
         <h1>Jotto</h1>
         <div>
-          <small>The secret word is {this.props.secretWord}</small>
+          <small>The secret word is {secretWord}</small>
         </div>
         <Congrats success={success} />
-        <NewWordButton display={success} resetAction={resetGame} />
+        <SecretWordReveal display={gaveUp} secretWord={secretWord} />
+        <NewWordButton display={success || gaveUp} resetAction={resetGame} />
         <Input />
         <GuessedWords guessedWords={guessedWords} />
         <TotalGuesses guessCount={guessedWords.length} />
@@ -36,8 +38,8 @@ export class UnconnectedApp extends Component {
 }
 
 const mapStateToProps = state => {
-  const { success, guessedWords, secretWord } = state;
-  return { success, guessedWords, secretWord };
+  const { success, guessedWords, secretWord, gaveUp } = state;
+  return { success, guessedWords, secretWord, gaveUp };
 };
 
 export default connect(mapStateToProps, { getSecretWord, resetGame })(
--- jotto-redux/src/components/Input/Input.js
+++ jotto-redux/src/components/Input/Input.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-import { guessWord } from "../../actions";
+import { guessWord, giveUp } from "../../actions";
 
 export class UnconnectedInput extends Component {
   inputBox = React.createRef();
 
-  submitGuessedWord = e => {
+  submitGuessedWord = evt => {
     const { guessWord } = this.props;
-    e.preventDefault();
+
+    evt.preventDefault();
     const guessedWord = this.inputBox.current.value;
 
     if (guessedWord && guessedWord.length > 0) {
@@ -18,34 +19,53 @@ export class UnconnectedInput extends Component {
     this.inputBox.current.value = "";
   };
 
+  giveUpOnClick = evt => {
+    const { giveUp } = this.props;
+
+    evt.preventDefault();
+    giveUp();
+  };
+
   render() {
-    const contents = this.props.success ? null : (
-      <form className="form-inline">
-        <input
-          ref={this.inputBox}
-          data-test="input-box"
-          className="mb-2 mx-sm-3"
-          id="word-guess"
-          type="text"
-          placeholder="enter guess"
-        />
-        <button
-          data-test="submit-button"
-          type="submit"
-          className="btn btn-primary mb-2"
-          onClick={this.submitGuessedWord}
-        >
-          Submit
-        </button>
-      </form>
-    );
+    const { success, gaveUp } = this.props;
+
+    const contents =
+      success || gaveUp ? null : (
+        <form className="form-inline">
+          <input
+            ref={this.inputBox}
+            data-test="input-box"
+            className="mb-2 mx-sm-3"
+            id="word-guess"
+            type="text"
+            placeholder="enter guess"
+          />
+          <button
+            data-test="submit-button"
+            type="submit"
+            className="btn btn-primary mb-2"
+            onClick={this.submitGuessedWord}
+          >
+            Submit
+          </button>
+          <button
+            data-test="give-up-button"
+            onClick={this.giveUpOnClick}
+            className="btn btn-danger mb-2 ml-2"
+          >
+            Give up
+          </button>
+        </form>
+      );
 
     return <div data-test="component-input">{contents}</div>;
   }
 }
 
-const mapStateTpProps = ({ success }) => {
-  return { success };
+const mapStateTpProps = ({ success, gaveUp }) => {
+  return { success, gaveUp };
 };
 
-export default connect(mapStateTpProps, { guessWord })(UnconnectedInput);
+export default connect(mapStateTpProps, { guessWord, giveUp })(
+  UnconnectedInput
+);
